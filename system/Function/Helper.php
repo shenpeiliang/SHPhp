4352653220f0e102c9ad2c4be089ad391bed95f0
shenpeiliang <2172592393@qq.com>
2019-12-11 17:20:06 +0800

    remove xss

--- system/Function/Helper.php
+++ system/Function/Helper.php
@@ -64,3 +64,28 @@ function frame_filter(string &$value)
 	}
 }
 
+/**
+ * 移除攻击代码
+ */
+function remove_xss($var)
+{
+	static $_parser = null;
+	if ($_parser === null)
+	{
+		require_once SRC_PATH . 'system/Vendor/htmlpurifier/HTMLPurifier.includes.php';
+		$config = HTMLPurifier_Config::createDefault();
+		$_parser = new HTMLPurifier ($config);
+	}
+	if (is_array($var))
+	{
+		foreach ($var as $key => $val)
+		{
+			$var [$key] = remove_xss($val);
+		}
+	} else
+	{
+		$var = $_parser->purify($var);
+	}
+	return $var;
+}
+
